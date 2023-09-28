464e20fca1b2e3d9a509e897223a62db5eb90100
sofiascharf <145060008+sofiascharf@users.noreply.github.com>
2023-09-28 16:10:55 +0200

    Add files via upload

---
+++ smiley.R
@@ -0,0 +1,6 @@
+plot(x=1, y=1, type='n', xlim=c(-2,2), ylim=c(-2,2))
+points(x=-1, y=1)
+points(x=1,y=1)
+segments(x0=-0.5, y0=-1, x1=0.5, y1=-1)
+segments(x0=-1, y0=0, x1=-0.5, y1=-1)
+segments(x0=0.5, y0=-1, x1=1, y1=0)
